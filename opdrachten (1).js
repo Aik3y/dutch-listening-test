4858a7aff8dcabd4b2bd4c4302a0445624f8608a
Aik3y <jeroen.aike@yahoo.com>
2025-05-06 16:43:54 +0200

    Add files via upload

---
+++ opdrachten (1).js
@@ -0,0 +1,59 @@
+const alleOpdrachten = [
+  {
+    "audio": "audio/makkelijk_01.mp3",
+    "niveau": "makkelijk",
+    "script": "Lisa gaat naar de supermarkt. Ze koopt brood en melk.",
+    "vragen": [
+      {
+        "vraag": "Waar gaat Lisa naartoe?",
+        "antwoord": "supermarkt"
+      },
+      {
+        "vraag": "Wat koopt Lisa?",
+        "antwoord": "brood"
+      },
+      {
+        "vraag": "Wat koopt Lisa nog meer?",
+        "antwoord": "melk"
+      }
+    ]
+  },
+  {
+    "audio": "audio/gemiddeld_01.mp3",
+    "niveau": "gemiddeld",
+    "script": "Pieter heeft morgen een afspraak bij de tandarts om half negen.",
+    "vragen": [
+      {
+        "vraag": "Wat heeft Pieter morgen?",
+        "antwoord": "afspraak"
+      },
+      {
+        "vraag": "Bij wie is de afspraak?",
+        "antwoord": "tandarts"
+      },
+      {
+        "vraag": "Hoe laat is de afspraak?",
+        "antwoord": "half negen"
+      }
+    ]
+  },
+  {
+    "audio": "audio/moeilijk_01.mp3",
+    "niveau": "moeilijk",
+    "script": "Tijdens het telefoongesprek vertelt Anne dat ze haar trein heeft gemist en later op het werk zal zijn.",
+    "vragen": [
+      {
+        "vraag": "Wat is er met Anne gebeurd?",
+        "antwoord": "trein gemist"
+      },
+      {
+        "vraag": "Wat vertelt Anne?",
+        "antwoord": "dat ze later is"
+      },
+      {
+        "vraag": "Waarom komt Anne later?",
+        "antwoord": "trein gemist"
+      }
+    ]
+  }
+];
